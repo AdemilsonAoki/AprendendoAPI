fe4454706a52bbb7a21a1f24ccf39a8307cf3a18
AdemilsonAoki <ademilson.aoki@gmail.com>
2020-12-07 13:33:12 -0300

    Push/get

---
+++ Models/InfectadoDto.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace AprendendoAPI.Models
+{
+    public class InfectadoDto
+    {
+        public DateTime DataNascimento { get; set; }
+        public string Sexo { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
